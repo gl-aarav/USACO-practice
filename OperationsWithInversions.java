20b2b75a415e7fb8bafc80f2b52dd57dcf033fe9
Aarav Goyal <gl.aarav@gmail.com>
2026-02-16 15:54:54 -0800

    changes

--- OperationsWithInversions.java
+++ OperationsWithInversions.java
@@ -1,3 +1,11 @@
-public class OperationsWithInversions {
+import java.util.Scanner;
 
+public class OperationsWithInversions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+
+        in.close();
+    }
 }
