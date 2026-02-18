ce11c2f8da6df16278fc14044a2ed172991a3fef
Aarav Goyal <gl.aarav@gmail.com>
2026-02-18 11:04:04 -0800

    update MooOperations.java

--- MooOperations.java
+++ MooOperations.java
@@ -4,37 +4,48 @@ public class MooOperations {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        if (!in.hasNextInt())
-            return;
-        int Q = in.nextInt();
+        if (in.hasNextInt()) {
+            int Q = in.nextInt();
 
-        while (Q-- > 0) {
-            String s = in.next();
-            int n = s.length();
-            int minOps = Integer.MAX_VALUE;
-
-            for (int i = 0; i <= n - 3; i++) {
-                String sub = s.substring(i, i + 3);
-
-                if (sub.charAt(1) == 'O') {
-                    int replacements = 0;
-
-                    if (sub.charAt(0) != 'M')
-                        replacements++;
-                    if (sub.charAt(2) != 'O')
-                        replacements++;
-
-                    int currentOps = (n - 3) + replacements;
-                    minOps = Math.min(minOps, currentOps);
-                }
-            }
-
-            if (minOps == Integer.MAX_VALUE) {
-                System.out.println("-1");
-            } else {
-                System.out.println(minOps);
+            for (int i = 0; i < Q; i++) {
+                String s = in.next();
+                System.out.println(solve(s));
             }
         }
         in.close();
+    }
+
+    private static int solve(String s) {
+        int n = s.length();
+
+        if (n < 3) {
+            return -1;
+        }
+
+        int minOps = Integer.MAX_VALUE;
+        boolean possible = false;
+
+        for (int i = 0; i <= n - 3; i++) {
+            if (s.charAt(i + 1) == 'O') {
+                possible = true;
+                int currentOps = n - 3;
+                if (s.charAt(i) != 'M') {
+                    currentOps++;
+                }
+
+                if (s.charAt(i + 2) != 'O') {
+                    currentOps++;
+                }
+
+                if (currentOps < minOps) {
+                    minOps = currentOps;
+                }
+            }
+        }
+
+        if (possible) {
+            return minOps;
+        }
+        return -1;
     }
 }
