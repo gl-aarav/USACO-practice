447b38b37151bf4e872a22e832a3002146da3e8b
Aarav Goyal <gl.aarav@gmail.com>
2026-02-18 23:06:50 -0800

    update Digits.java

---
+++ PrintingSequences.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PrintingSequences {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        if (in.hasNextInt()) {
+            int T = in.nextInt();
+            while (T-- > 0) {
+
+            }
+        }
+        in.close();
+    }
+}
