f26742d5b2ed8f98458c5f02d58c76238bd79baf
Aarav Goyal <gl.aarav@gmail.com>
2026-02-20 09:00:22 -0800

    Changes

---
+++ PhotoShoot.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PhotoShoot {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        in.close();
+    }
+}
