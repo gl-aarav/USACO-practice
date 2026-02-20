65353442c53054db84d6e02f774f3bf8b595c5e2
Aarav Goyal <gl.aarav@gmail.com>
2026-02-20 09:06:05 -0800

    updates

--- PhotoShoot.java
+++ PhotoShoot.java
@@ -15,31 +15,6 @@ public class PhotoShoot {
 
         long currentMax = 0;
 
-        for (int i = 0; i < q; i++) {
-            int r = in.nextInt();
-            int c = in.nextInt();
-            long v = in.nextLong();
-
-            long delta = v - grid[r][c];
-            grid[r][c] = v;
-
-            int startRow = Math.max(1, r - k + 1);
-            int endRow = Math.min(n - k + 1, r);
-            int startCol = Math.max(1, c - k + 1);
-            int endCol = Math.min(n - k + 1, c);
-
-            for (int row = startRow; row <= endRow; row++) {
-                for (int col = startCol; col <= endCol; col++) {
-                    sum[row][col] += delta;
-                    if (sum[row][col] > currentMax) {
-                        currentMax = sum[row][col];
-                    }
-                }
-            }
-
-            System.out.println(currentMax);
-        }
-
         in.close();
     }
 }
